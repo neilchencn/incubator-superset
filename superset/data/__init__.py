b633282cf87ebda75568e8d4518e01c726edd884
neil.chen.nj@gmail.com <neil.chen.nj@gmail.com>
2018-03-21 10:13:39 +0800

    add load dictionary and load company

--- superset/data/__init__.py
+++ superset/data/__init__.py
@@ -10,7 +10,7 @@ import json
 import os
 import random
 import textwrap
-
+import sqlite3
 import pandas as pd
 from sqlalchemy import BigInteger, Date, DateTime, Float, String, Text, Integer
 import geohash
@@ -1852,6 +1852,7 @@ def load_dictionarys():
                 ids.append(idx + 1)
                 idx += 1
             df = pd.DataFrame(dict(id=ids, field_name=names, describe=des))
+            df = clean_df_db_dups(df, tbl_name, db.engine, dup_cols=['field_name','describe'])
             df.to_sql(
                 tbl_name,
                 db.engine,
@@ -1886,6 +1887,52 @@ def byteify(input):
         return input
 
 
+def clean_df_db_dups(df, tablename, engine, dup_cols=[],
+                     filter_continuous_col=None, filter_categorical_col=None):
+    """
+    Remove rows from a dataframe that already exist in a database
+    Required:
+        df : dataframe to remove duplicate rows from
+        engine: SQLAlchemy engine object
+        tablename: tablename to check duplicates in
+        dup_cols: list or tuple of column names to check for duplicate row values
+    Optional:
+        filter_continuous_col: the name of the continuous data column for BETWEEEN min/max filter
+                               can be either a datetime, int, or float data type
+                               useful for restricting the database table size to check
+        filter_categorical_col : the name of the categorical data column for Where = value check
+                                 Creates an "IN ()" check on the unique values in this column
+    Returns
+        Unique list of values from dataframe compared to database table
+    """
+    args = 'SELECT %s FROM %s' % (
+        ', '.join(['"{0}"'.format(col) for col in dup_cols]), tablename)
+    args_contin_filter, args_cat_filter = None, None
+    if filter_continuous_col is not None:
+        if df[filter_continuous_col].dtype == 'datetime64[ns]':
+            args_contin_filter = """ "%s" BETWEEN Convert(datetime, '%s')
+                                          AND Convert(datetime, '%s')""" % (filter_continuous_col,
+                                                                            df[filter_continuous_col].min(), df[filter_continuous_col].max())
+
+    if filter_categorical_col is not None:
+        args_cat_filter = ' "%s" in(%s)' % (filter_categorical_col,
+                                            ', '.join(["'{0}'".format(value) for value in df[filter_categorical_col].unique()]))
+
+    if args_contin_filter and args_cat_filter:
+        args += ' Where ' + args_contin_filter + ' AND' + args_cat_filter
+    elif args_contin_filter:
+        args += ' Where ' + args_contin_filter
+    elif args_cat_filter:
+        args += ' Where ' + args_cat_filter
+
+    df.drop_duplicates(dup_cols, keep='last', inplace=True)
+    df = pd.merge(df, pd.read_sql(args, engine),
+                  how='left', on=dup_cols, indicator=True)
+    df = df[df['_merge'] == 'left_only']
+    df.drop(['_merge'], axis=1, inplace=True)
+    return df
+
+
 def load_companies():
     tbl_name = 'company'
     with open(os.path.join(DATA_FOLDER, 'company.json')) as f:
@@ -1895,6 +1942,7 @@ def load_companies():
         data += [v for d, v in byteify(json.load(bs)).items()]
 
     df = pd.DataFrame(dict(id=range(1, len(data) + 1), field_name=data))
+    df = clean_df_db_dups(df, tbl_name, db.engine, dup_cols=['field_name'])
     df.to_sql(
         tbl_name,
         db.engine,
