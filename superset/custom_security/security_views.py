2c1801206879623ac6277fea5d313d189ef09d27
neil.chen.nj@gmail.com <neil.chen.nj@gmail.com>
2018-06-21 14:53:04 +0800

    add greent_asia and irt_asia hourly datasource

--- superset/custom_security/security_views.py
+++ superset/custom_security/security_views.py
@@ -178,6 +178,10 @@ class MyAuthRemoteUserView(AuthDBView):
                     if pname == 'iRT':
                         roles.append('DATASOURCE[iRT-hourly]')
                         roles.append('DICT[iRT]')
+                    if pname == 'GreenT_asia':
+                        roles.append('DATASOURCE[GreenT-asia-hourly]')
+                    if pname == 'iRT_asia':
+                        roles.append('DATASOURCE[iRT-aisa-hourly]')
                     if pname == 'DSE':
                         roles.append('DICT[DSE]')
                     if pname == 'DSED':
