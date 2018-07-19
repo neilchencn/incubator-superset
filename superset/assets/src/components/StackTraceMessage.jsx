1c7a15e0c9b2c14494a4d288c3e023911d94a88d
neil.chen.nj@gmail.com <neil.chen.nj@gmail.com>
2018-07-19 06:13:30 +0000

    modify error message dev can auto reset size when change web page size, and can scroll;

--- superset/assets/src/components/StackTraceMessage.jsx
+++ superset/assets/src/components/StackTraceMessage.jsx
@@ -18,29 +18,61 @@ class StackTraceMessage extends React.PureComponent {
 
     this.state = {
       showStackTrace: props.showStackTrace,
+      height: 500,
     };
+  }
+
+  /**
+     * Add event listener
+     */
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  /**
+     * Remove event listener
+     */
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions.bind(this));
   }
 
   hasTrace() {
     return this.props.queryResponse && this.props.queryResponse.stacktrace;
   }
 
+  /**
+     * Calculate & Update state of new dimensions
+     */
+  updateDimensions() {
+    if (this.state.showStackTrace) {
+      this.setState({ height: window.innerHeight - 250 });
+    }
+  }
+
   render() {
     return (
-      <div className={`stack-trace-container${this.hasTrace() ? ' has-trace' : ''}`}>
+      <div
+        className={`stack-trace-container${this.hasTrace() ? ' has-trace' : ''}`}
+      >
         <Alert
           bsStyle="warning"
-          onClick={() => this.setState({ showStackTrace: !this.state.showStackTrace })}
+          onClick={() =>
+            this.setState({
+              showStackTrace: !this.state.showStackTrace,
+              height: window.innerHeight - 250,
+            })}
         >
           {this.props.message}
         </Alert>
         {this.hasTrace() &&
           <Collapse in={this.state.showStackTrace}>
-            <pre>
-              {this.props.queryResponse.stacktrace}
-            </pre>
-          </Collapse>
-        }
+            <div>
+              <pre style={{ height: this.state.height || 500 }}>
+                {this.props.queryResponse.stacktrace}
+              </pre>
+            </div>
+          </Collapse>}
       </div>
     );
   }
